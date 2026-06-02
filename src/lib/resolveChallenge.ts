e64cb58024c102e5ed211cc263753a860fbcf4c7
Nick Perry <nick_perry@clinicalarchitecture.com>
2026-06-02 10:57:34 -0400

    launch.json changes

--- src/lib/resolveChallenge.ts
+++ src/lib/resolveChallenge.ts
@@ -17,7 +17,6 @@ export async function resolveChallenge(key: string) {
     const guild = client?.guilds.cache.get(process.env.GUILD_ID!);
     if(!guild) return;
 
-    // const logChannel = guild?.channels.cache.get(process.env.LOG_CHANNEL_ID!);
     const logChannel = await guild?.channels
         .fetch(process.env.LOG_CHANNEL_ID!)
         .catch(() => null);
