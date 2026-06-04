a982abddd9d19b0f2f0a1712b0b4d9f46c937418
Nick <nicholasjperry1@gmail.com>
2026-06-04 01:22:31 -0400

    better-sqlite3

--- src/index.ts
+++ src/index.ts
@@ -26,15 +26,4 @@ const client = new SapphireClient({
 
 (globalThis as { client?: SapphireClient }).client = client;
 
-// cron.schedule('0 0 * * *', async () => {
-//     const guild = client.guilds.cache.get(process.env.GUILD_ID!);
-//     const logChannel = guild?.channels.cache.find(c => c.name === 'challenge-log');
-
-//     if (!logChannel?.isTextBased()) return;
-
-//     const messages = await logChannel?.messages.fetch({ limit: 100});
-
-//     messages.forEach(m => m.delete());
-// });
-
 client.login(process.env.TOKEN);
